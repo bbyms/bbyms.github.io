04ae45f591d5b69e9c5b843555a0331502b6dfce
barbara mendes <barbara.mendes@bitmaker-software.com>
2016-06-03 12:53:24 +0100

    Conor palette from image

---
+++ sw.js
@@ -0,0 +1,39 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('static-v3')
+    .then(cache => cache.addAll([
+      '/offline.html',
+      '/css/main.css'
+    ]))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => response || fetch(event.request))
+    .catch(() => {
+        if (event.request.mode == 'navigate') {
+          return caches.match('offline.html');
+        }
+    })
+  );
+});
+
+const expectedCaches = [
+  'static-v3'
+];
+
+self.addEventListener('active', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promisse.all(
+        cacheNames.map(cacheName => {
+          if(!expectedCaches.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    })
+  )
+});
