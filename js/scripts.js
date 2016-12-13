1db6465f31576b52b8c6f825a7d49f8670e23b91
Barbara Mendes <barbara.mendes@bitmaker-software.com>
2016-12-13 23:33:11 +0000

    form

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,29 @@ if (navigator.serviceWorker) {
 $('#reader').click(function() {
   $('body').toggleClass('reader');
 });
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+var formData = JSON.stringify($("#post").serializeArray());
+
+$.ajax({
+  type: "POST",
+  url: "quote.html",
+  data: formData,
+  success: function(){},
+  dataType: "json",
+  contentType : "application/json"
+});
